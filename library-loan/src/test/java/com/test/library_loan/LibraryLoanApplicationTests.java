190b7cf5321a43f1a53640b6457d4edf035e2de3
papaursa888 <papaursa888@gmail.com>
2026-03-29 14:07:39 +0700

    initiate project

---
+++ library-loan/src/test/java/com/test/library_loan/LibraryLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.library_loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
